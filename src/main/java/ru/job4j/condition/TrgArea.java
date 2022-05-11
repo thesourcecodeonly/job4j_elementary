037324253d265ac62d97cae68d76908d9fa907c7
vyacheslav833 <zv.specialone@gmail.com>
2022-05-11 09:05:07 +0300

    9. Площадь треугольника. [#175474 #299430] - исправление, удаление промежуточной переменной double rsl

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -4,8 +4,7 @@ public class TrgArea {
 
     public static double area(double a, double b, double c) {
         double p = (a + b + c) / 2;
-        double sqr = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return  sqr;
+        return  Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
