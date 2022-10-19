677bf3c310b90d14d097a50f692a704c4edd35ad
vyacheslav833 <zv.specialone@gmail.com>
2022-10-19 14:35:15 +0300

    4.5. Отладка программы в IDEA [#159464]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 public class SqArea {
 
     public static double square(double p, double k) {
+
         double h = p / (2 * (k + 1));
         double l = h * k;
         double sqr = l * h;
---
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class ThreeMax {
+
+    public  static int max(int first, int second, int third) {
+        int result = third;
+        if ((first > second && first > third) || (first == second && first > third)) {
+            result = first;
+        }
+        if (second > first && second > third) {
+            result = second;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ThreeMax.max(5, 7, 12));
+    }
+}
