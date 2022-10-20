0934620eedd2df5d848a15c9dcfb1b7537607348
vyacheslav833 <zv.specialone@gmail.com>
2022-10-20 14:17:48 +0300

    5.4. Протеиновая диета [#156313 #353457] - corrected

--- src/main/java/ru/job4j/condition/loop/Fitness.java
+++ src/main/java/ru/job4j/condition/loop/Fitness.java
@@ -4,12 +4,7 @@ public class Fitness {
 
     public static int calc(int ivan, int nik) {
         int month = 0;
-        if (ivan > nik) {
-            return 0;
-        } else if (ivan == nik) {
-            return 1;
-        }
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
             month++;
